8e59928dd276f3e4a20c4340dd0a404cbebd9d16
venka <venkat.ranga.k@gmail.com>
2021-09-21 21:13:09 +0530

    first commit

---
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class EmployeeController {
+  
+  @GetMapping("/demo/{name}")
+  private String demo(@PathVariable String name) {
+    
+    return "Test Application, Hello "+name;
+  }
+
+}
