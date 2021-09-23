9c160729409c53d1bb9baf9ad6974e3c998e3edb
venka <venkat.ranga.k@gmail.com>
2021-09-23 20:44:52 +0530

    changes made

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
